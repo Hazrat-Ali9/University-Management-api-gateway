6c3f034a655fa84d84f109665706a7238ef4de60
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-10 22:09:45 +0600

    semester payment 🤯

--- src/app/modules/studentSemesterPayment/studentSemesterPayment.routes.ts
+++ src/app/modules/studentSemesterPayment/studentSemesterPayment.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { StudentSemesterPaymentController } from './studentSemesterPayment.controller';
-
+// Students semester payment
 const router = express.Router();
 
 router.get('/my-semester-payments', StudentSemesterPaymentController.getMyPayment);
